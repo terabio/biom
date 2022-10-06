805580de3ae6c9c2236fd1d578fbe4e2e668feab
Aleksandr Fedorov <anfedorov@hse.ru>
2022-10-06 07:46:06 +0300

    WIP

--- src/biom/annotate/annotate.py
+++ src/biom/annotate/annotate.py
@@ -1,24 +1,25 @@
+from HTSeq import GenomicArrayOfSets, GenomicInterval
 from collections import defaultdict
 from typing import Dict, Optional, Set, Tuple
-
-from HTSeq import GenomicArrayOfSets, GenomicInterval
 
 from .. import ensembl, repmasker
 
 
-def byrepeats(repeats: GenomicArrayOfSets, key: GenomicInterval, how: str = "name") -> Dict[str, float]:
+def byrepeats(classification: repmasker.RepmaskerClassification,
+              repeats: GenomicArrayOfSets, key: GenomicInterval, how: str = "name") -> Dict[str, float]:
     assert how in {"name", "class", "family"}
     ind = {"name": 0, "family": 1, "class": 2}[how]
     weights = defaultdict(int)
-    for iv, overlaps in repeats[key].steps():
-        if iv.length > 0:
-            if len(overlaps) == 1:
-                for name, _ in overlaps:
-                    weights[repmasker.classify(name)[ind]] += iv.length
-            else:
-                # Same overlap -> fractional weights
-                for name, _ in overlaps:
-                    weights[repmasker.classify(name)[ind]] += iv.length / len(overlaps)
+    if key.chrom in repeats.chrom_vectors:
+        for iv, overlaps in repeats[key].steps():
+            if iv.length > 0:
+                if len(overlaps) == 1:
+                    for name, _ in overlaps:
+                        weights[classification.classify(name)[ind]] += iv.length
+                else:
+                    # Same overlap -> fractional weights
+                    for name, _ in overlaps:
+                        weights[classification.classify(name)[ind]] += iv.length / len(overlaps)
     covered = sum(weights.values())
 
     if covered < key.length:
@@ -32,8 +33,9 @@ def byrepeats(repeats: GenomicArrayOfSets, key: GenomicInterval, how: str = "nam
 
 
 def bytranscriptome(
+        assembly: ensembl.EnsemblAssembly,
         trindex: GenomicArrayOfSets, key: GenomicInterval, strand: str, expressed: Optional[Set[str]] = None,
-        priority: Tuple[str, ...] = ("cds", "utr3", "utr5", "exon", "intron", "downstream+10k")
+        priority: Tuple[str, ...] = ("cds", "utr3", "utr5", "exon", "intron", "downstream+10k",),
 ) -> Tuple[Dict[str, float], Dict[str, Set[str]]]:
     assert strand in ("+", "-")
     priority = {k: ind for ind, k in enumerate(priority)}
@@ -46,7 +48,8 @@ def bytranscriptome(
                 options = defaultdict(set)
                 for feature, trid, trstrand in transcripts:
                     if trstrand == strand:
-                        biotype = ensembl.transcripts.id2biotype(trid)
+                        biotype = assembly.transcripts.id2biotype(trid)
+                        assert biotype is not None, f"Unknown transcript ID: {trid}"
                         if (biotype == "protein_coding" or biotype == "lncRNA") and \
                                 (expressed is None or trid in expressed):
                             options[feature].add(trid)
@@ -54,7 +57,6 @@ def bytranscriptome(
                         elif biotype == "miRNA" or biotype == "scRNA" or biotype == "snoRNA" or biotype == "ncRNA":
                             options[feature].add(trid)
                         else:
-                            print("UNKNWOWN BIOTYPE!!!")
                             options[feature].add(trid)
                 if options:
                     feature, trids = min(options.items(), key=lambda x: priority[x[0]])
