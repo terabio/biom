c0a76c7d7fc7eed588168fc439f8bb8a70237176
alnfedorov <18511496+alnfedorov@users.noreply.github.com>
2023-11-06 11:51:21 +0000

    s/f stranding protocol

--- src/biom/ripper/core/fragments/strdeductors.py
+++ src/biom/ripper/core/fragments/strdeductors.py
@@ -8,6 +8,10 @@ StrandDeductor = Callable[[AlignedSegment, AlignedSegment], str]
 def get(protocol: str):
     if protocol == "f/s":
         return _fs
+    elif protocol == "s/f":
+        return _sf
+    else:
+        raise ValueError(f"Unknown stranding protocol: {protocol}")
 
 
 def _fs(lmate: AlignedSegment, rmate: AlignedSegment) -> str:
@@ -20,3 +24,15 @@ def _fs(lmate: AlignedSegment, rmate: AlignedSegment) -> str:
     else:
         assert rmate.is_reverse
         return "-"
+
+
+def _sf(lmate: AlignedSegment, rmate: AlignedSegment) -> str:
+    assert lmate.is_read1 and rmate.is_read2
+    # (-/+) -> -
+    if lmate.is_reverse:
+        assert not rmate.is_reverse
+        return "-"
+    # (+/-) -> +
+    else:
+        assert rmate.is_reverse
+        return "+"
