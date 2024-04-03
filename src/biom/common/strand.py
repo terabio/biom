99e8effe724275cc5af4a047bd8323a390c51206
alnfedorov <18511496+alnfedorov@users.noreply.github.com>
2024-04-03 14:07:51 +0100

    Fix tests

--- src/biom/common/strand.py
+++ src/biom/common/strand.py
@@ -20,6 +20,9 @@ class Strand(Enum):
     def __hash__(self) -> int:
         return hash(self.value)
 
+    def __repr__(self):
+        return self.value
+
     def __str__(self):
         return self.value
 
