b803c7efd41091e5426945a550221336cebfbc20
alnfedorov <18511496+alnfedorov@users.noreply.github.com>
2024-04-15 20:13:44 +0100

    Refactor the counting

--- src/biom/analysis/rcount/__init__.py
+++ src/biom/analysis/rcount/__init__.py
@@ -1,5 +1,6 @@
 from . import source, resolve
+from .counter import MultiReadsCounter
 from .resolve import Resolution
 from .source import Source
 
-__all__ = ['source', 'resolve', 'Source', 'Resolution']
+__all__ = ['source', 'resolve', 'counter', 'Source', 'Resolution', 'MultiReadsCounter']
---
+++ src/biom/analysis/rcount/counter/__init__.py
@@ -0,0 +1,5 @@
+from .joblib_counter import JoblibMultiReadsCounter
+from .partition import Partition
+from .reads_counter import MultiReadsCounter
+
+__all__ = ["Partition", "MultiReadsCounter", "JoblibMultiReadsCounter"]
